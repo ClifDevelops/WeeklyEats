3a2055f4903f10992005d789346bd9968f5cde7b
Robert Burroughs <clifdevelops@gmail.com>
2021-05-06 17:21:08 -0600

    “added”

--- app/api/meal_routes.py
+++ app/api/meal_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import Ingredient, Meal, db
-from flask_login import login_required
+from app.models import Ingredient, Meal, UserMeal, db
+from flask_login import login_required, current_user
 from app.forms import MealForm
 
 meal_routes = Blueprint('meals', __name__)
@@ -8,7 +8,9 @@ meal_routes = Blueprint('meals', __name__)
 @meal_routes.route('/', methods=['POST'])
 @login_required
 def addToMeals():
+    userId = current_user.id
     form = MealForm()
+    # meal_name = form.data['name'] 
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         meal = Meal(
@@ -18,5 +20,15 @@ def addToMeals():
         )
         db.session.add(meal)
         db.session.commit()
+
+        new_meal = Meal.query.filter(Meal.name == form.data['name']).first()
+        new_meal_dict = new_meal.to_dict()
+        # print('!!!!!!!!!!!!!!!!!!!!!!!', new_meal_dict)
+        user_meal = UserMeal(user_id=userId, meal_id=new_meal_dict['id'])
+        # print(user_meal)
+
+        db.session.add(user_meal)
+        db.session.commit()
         return meal.to_dict()
+
     return 'Hello'
