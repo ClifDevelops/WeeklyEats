d9d49bc7a551a7459e057060499bf4995a678cfe
Robert Burroughs <clifdevelops@gmail.com>
2021-05-11 18:17:08 -0600

    “some”

--- react-app/src/components/MealDisplay/index.js
+++ react-app/src/components/MealDisplay/index.js
@@ -6,6 +6,7 @@ import { getAllMeals } from "../../store/meal";
 import { deleteFromGroceryList, addToGroceryList } from "../../store/grocerylist";
 import { getAllIngredients } from "../../store/ingredient";
 import MealIngredientsForm from "../MealIngredientsForm";
+import './MealDisplay.css'
 
 const MealDisplay = () => {
     const dispatch = useDispatch();
@@ -13,8 +14,8 @@ const MealDisplay = () => {
     const { mealId } = useParams();
     const meal = useSelector(state => state?.meal[mealId])
     const ingredients = useSelector((state) => state?.ingredient?.ingredients);
-    console.log('HERE IT IS', meal?.meal_ingredients.forEach(ingredient => ingredient.ingredient['name']))
-    console.log("THE NEXT ONE", meal?.meal_ingredients[0].ingredient.name);
+    // console.log('HERE IT IS', meal?.meal_ingredients.forEach(ingredient => ingredient.ingredient['name']))
+    // console.log("THE NEXT ONE", meal?.meal_ingredients[0].ingredient.name);
 
      useEffect(() => {
        dispatch(getAllMeals());
@@ -30,24 +31,25 @@ const MealDisplay = () => {
     return (
       <div>
         <div className="meal-display-container">
-          <div>{meal?.name}</div>
-          <div>Cuisine: {meal?.cuisine}</div>
-          <div>INGREDIENTS</div>
-          {/* <div>{meal?.meal_ingredients[0].ingredient.name}</div> */}
-          <div>{meal?.meal_ingredients.map((ingredient) => {
-              return (
-                  <div>{ingredient.ingredient.name}</div>
-              )
-          })}</div>
-          <div> RECIPE</div>
-          <div>{meal?.recipe}</div>
-          <button> Edit Meal</button>
-          <button> Add ingredients to meal </button>
+          <div className='meal-container'>
+            <div className='meal-display-name'>{meal?.name}</div>
+            <div className='meal-display-cuisine'>Cuisine: {meal?.cuisine}</div>
+            <div className='meal-display-recipe-title'> RECIPE</div>
+            <div className='meal-display-recipe'>{meal?.recipe}</div>
+            <button className='meal-edit-button'> Edit Meal</button>
+          </div>
+          <div className='meal-display-ingredients-container'>
+            <div>INGREDIENTS</div>
+            {/* <div>{meal?.meal_ingredients[0].ingredient.name}</div> */}
+            <div>{meal?.meal_ingredients.map((ingredient) => {
+                return (
+                    <div>{ingredient.ingredient.name}</div>
+                )})}</div>
+            <MealIngredientsForm mealId={mealId} />
+          </div>
         </div>
-        <MealIngredientsForm mealId={mealId} />
       </div>
     );
-
 }
 
 export default MealDisplay
--- react-app/src/components/MealIngredientsForm/index.js
+++ react-app/src/components/MealIngredientsForm/index.js
@@ -43,64 +43,66 @@ const MealIngredientsForm = ({mealId}) => {
      };
     if (ingredients) {
     return (
-      <form onSubmit={onSubmit}>
-        <div>
+      <div className='meal-ingredient-form-container'>
+        <form onSubmit={onSubmit}>
           <div>
-            <label htmlFor="ingredient_id">Ingredient</label>
+            <div>
+              <label htmlFor="ingredient_id">Ingredient</label>
+            </div>
+            <div>
+              <select
+                name="ingredient_id"
+                value={ingredient_id}
+                onChange={(e) => setIngredient_id(e.target.value)}
+              >
+                {Object.values(ingredients).map((ingredient) => {
+                  return (
+                    <option value={ingredient?.id}>{ingredient?.name}</option>
+                  );
+                })}
+              </select>
+            </div>
+            {/* <div>
+              <label htmlFor="ingredient_quantity">Ingredient Quantity</label>
+            </div>
+            <div>
+              <input
+                onChange={(e) => setIngredient_quantity(e.target.value)}
+                name="ingredient_quantity"
+                type="number"
+                step="0.5"
+                className="ingredient-quantity-input"
+              ></input>
+            </div>
+            <div>
+              <label htmlFor="measurement_unit">Measurement Unit</label>
+            </div>
+            <div>
+              <select
+                name="measurement_unit"
+                onChange={updateMeasurementUnit}
+                value={measurement_unit}
+              >
+                <option value="tsp">tsp</option>
+                <option value="tbsp">tbsp</option>
+                <option value="fl oz">fl oz</option>
+                <option value="c">c</option>
+                <option value="pt">pt</option>
+                <option value="qt">qt</option>
+                <option value="mL">mL</option>
+                <option value="L">L</option>
+                <option value="lb">lb</option>
+                <option value="oz">oz</option>
+                <option value="g">g</option>
+                <option value="mg">mg</option>
+                <option value="kg">kg</option>
+                <option value="each">each</option>
+              </select>
+            </div> */}
           </div>
-          <div>
-            <select
-              name="ingredient_id"
-              value={ingredient_id}
-              onChange={(e) => setIngredient_id(e.target.value)}
-            >
-              {Object.values(ingredients).map((ingredient) => {
-                return (
-                  <option value={ingredient?.id}>{ingredient?.name}</option>
-                );
-              })}
-            </select>
-          </div>
-          {/* <div>
-            <label htmlFor="ingredient_quantity">Ingredient Quantity</label>
-          </div>
-          <div>
-            <input
-              onChange={(e) => setIngredient_quantity(e.target.value)}
-              name="ingredient_quantity"
-              type="number"
-              step="0.5"
-              className="ingredient-quantity-input"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="measurement_unit">Measurement Unit</label>
-          </div>
-          <div>
-            <select
-              name="measurement_unit"
-              onChange={updateMeasurementUnit}
-              value={measurement_unit}
-            >
-              <option value="tsp">tsp</option>
-              <option value="tbsp">tbsp</option>
-              <option value="fl oz">fl oz</option>
-              <option value="c">c</option>
-              <option value="pt">pt</option>
-              <option value="qt">qt</option>
-              <option value="mL">mL</option>
-              <option value="L">L</option>
-              <option value="lb">lb</option>
-              <option value="oz">oz</option>
-              <option value="g">g</option>
-              <option value="mg">mg</option>
-              <option value="kg">kg</option>
-              <option value="each">each</option>
-            </select>
-          </div> */}
-        </div>
-        <button type="submit">Add to Meal</button>
-      </form>
+          <button type="submit">Add to Meal</button>
+        </form>
+      </div>
     );
   } else {
     return (
